12a7c069fbbe49fdac212da0e6ac0b80f889db2c
ktkrau <ktkrau@gmail.com>
2022-03-05 17:19:22 +0000

    estructura vista portal terminada

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,18 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import Card from "../component/card.js";
+import Buscador from "../component/buscador.js";
+import Paginas from "../component/paginas.js"
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
-
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+		<>
+			<div className="container ">
+				<Buscador />
+				<Card />
+				<Paginas />
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
-		</div>
+		</>
 	);
 };
