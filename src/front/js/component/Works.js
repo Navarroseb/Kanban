e59ffceb9471598ce5a0ebaf60ecab623c0af11d
kao-hurtado22 <kao.hurtado22@gmail.com>
2022-03-06 06:46:26 +0000

    avance 1.3

--- src/front/js/component/Works.js
+++ src/front/js/component/Works.js
@@ -1,39 +1,33 @@
-import React from "react";
+/* import React from "react";
 import PropTypes from "prop-types";
-import Rigo from "../../img/rigo-baby.jpg";
-import { FaEnvelope } from "react-icons/fa";
-import { FaWhatsapp } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
+import Codigo from "../../img/codigo.jpg";
+import { FaBriefcase } from "react-icons/fa";
 
-const Card = (props) => {
+
+const Work = (props) => {
     return (
         <> <div className="container">
-            <div className="card mb-3">
+            <div className="work mb-3">
                 <div className="row g-0">
                     <div className="col-md-4">
-                        <img src="https://i.pinimg.com/564x/49/df/07/49df07ffea248dfca6a579e9096fa04f.jpg" className="img-fluid rounded-start" alt="..." />
+                        <img src={Codigo} className="img-fluid rounded-start" alt="..." />
                     </div>
                     <div className="col-md-8">
                         <div className="card-body">
-                            <h3 className="card-title">{props.title}</h3>
-                            <h5 className="card-title py-3">{props.title2}</h5>
-                            <p className="card-text">
-                                {props.description}
-                            </p>
+                            <h1 className="card-title">{props.title}</h1>
                             <p className="card-text">
                                 <ul className="list-group">
                                     <li className="list-group-item list-group-item-action animate__animated animate__backInRight animate__delay-1s">
-                                        <FaEnvelope /> Kao.hurtado22@gmail.com
+                                        <FaBriefcase /> <a href="https://github.com/kao-hurtado22" target="_blank" >Proyecto kathy</a>
                                     </li>
                                     <li className="list-group-item list-group-item-action animate__animated animate__backInRight animate__delay-2s">
-                                        <FaWhatsapp /> +56988847176
+                                        <FaBriefcase /> <a href="https://github.com/kao-hurtado22" target="_blank" >Proyecto Migue</a>
                                     </li>
                                     <li className="list-group-item list-group-item-action animate__animated animate__backInRight animate__delay-3s">
-                                        <FaLinkedin /> <a href="https://www.linkedin.com/in/ricardo-hurtado-409035226/" target="_blank" >Ricardo Hurtado</a>
+                                        <FaLinkedin /> <a href="https://github.com/kao-hurtado22" target="_blank" >Proyecto Seba</a>
                                     </li>
                                     <li className="list-group-item list-group-item-action animate__animated animate__backInRight animate__delay-4s">
-                                        <FaGithub /> <a href="https://github.com/kao-hurtado22" target="_blank" >Ricardo Hurtado (Git Hub)</a>
+                                        <FaBriefcase /> <a href="https://github.com/kao-hurtado22" target="_blank" >Proyecto kao</a>
                                     </li>
                                 </ul>
                             </p>
@@ -47,15 +41,13 @@ const Card = (props) => {
     );
 };
 
-Card.propTypes = {
+Work.propTypes = {
     title: PropTypes.string,
-    title2: PropTypes.string,
-    description: PropTypes.string,
-    buttonLabel: PropTypes.string,
 };
 
-export default Card;
+export default Work;
 
 
 
 
+ */
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -5,55 +5,66 @@ import "../../styles/home.css";
 import Breadcrum from "../component/Breadcrum";
 import Card from "../component/Cards";
 import Service from "../component/Service";
+/* import Work from "../component/Works"; */
 
 
 
 export const Home = () => {
 	return (
 		<>
-			<div className="container">
-				<Breadcrum />
-			</div>
-			<div className="container">
-				<div className="row">
-					<div className="col-md-12 py-5">
-						<Card
-							title="Titulo"
-							description="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-							description2="Last updated 3 mins ago"
-							buttonLabel="Learn More"
-						/>
+			<section id="header">
+				<div className="container py-3">
+					<Breadcrum />
+				</div>
+				<div className="container">
+					<div className="row">
+						<div className="col-md-12 py-5">
+							<Card
+								title="Ricardo Hurtado Lastra"
+								title2="Biografia"
+								description="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+							/>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div className="container">
-				<div className="row">
-					<div className="col-md-6 text-center" id="service-1">
-						<Service
-							title="Service"
-							description="“Ser Full Stack Developer no implica dominar por completo cada aspecto del front-end o el back-end, pero sí poder trabajar en ambos lados, comunicarlos y entender lo que sucede cuando se crea una aplicación”"
-						/>
-					</div>
-					<div className="col-md-6 text-center" id="service-2">
-						<Service
-							title="Años de experiencia"
-							description="“Ser Full Stack Developer no implica dominar por completo cada aspecto del front-end o el back-end, pero sí poder trabajar en ambos lados, comunicarlos y entender lo que sucede cuando se crea una aplicación”"
-						/>
-					</div>
-					<div className="col-md-6 text-center py-5" id="service-3">
-						<Service
-							title="Mis Habilidad"
-							description="“Ser Full Stack Developer no implica dominar por completo cada aspecto del front-end o el back-end, pero sí poder trabajar en ambos lados, comunicarlos y entender lo que sucede cuando se crea una aplicación”"
-						/>
-					</div>
-					<div className="col-md-6 text-center py-5" id="service-4">
-						<Service
-							title="Mis Portafolios"
-							description="“Ser Full Stack Developer no implica dominar por completo cada aspecto del front-end o el back-end, pero sí poder trabajar en ambos lados, comunicarlos y entender lo que sucede cuando se crea una aplicación”"
-						/>
+			</section>
+			<section id="body-1">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-6 text-center" id="service-1">
+							<Service
+								title="Servicios"
+								description="“Ser Full Stack Developer no implica dominar por completo cada aspecto del front-end o el back-end, pero sí poder trabajar en ambos lados, comunicarlos y entender lo que sucede cuando se crea una aplicación”"
+							/>
+						</div>
+						<div className="col-md-6 text-center" id="service-2">
+							<Service
+								title="Años de experiencia"
+								description="“Ser Full Stack Developer no implica dominar por completo cada aspecto del front-end o el back-end, pero sí poder trabajar en ambos lados, comunicarlos y entender lo que sucede cuando se crea una aplicación”"
+							/>
+						</div>
+						<div className="col-md-6 text-center py-5" id="service-3">
+							<Service
+								title="Mis Habilidad"
+								description="“Ser Full Stack Developer no implica dominar por completo cada aspecto del front-end o el back-end, pero sí poder trabajar en ambos lados, comunicarlos y entender lo que sucede cuando se crea una aplicación”"
+							/>
+						</div>
+						<div className="col-md-6 text-center py-5" id="service-4">
+							<Service
+								title="Mis Portafolios"
+								description="“Ser Full Stack Developer no implica dominar por completo cada aspecto del front-end o el back-end, pero sí poder trabajar en ambos lados, comunicarlos y entender lo que sucede cuando se crea una aplicación”"
+							/>
+						</div>
 					</div>
 				</div>
-			</div>
+			</section>
+			{/* <section id="body-2">
+				<div className="container">
+					<Work
+						title="Mis Trabajos"
+					/>
+				</div>
+			</section> */}
 		</>
 	);
 };
