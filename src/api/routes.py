f842b4a919cad61cc2ef8213ce39fe5226fbe564
kao-hurtado22 <kao.hurtado22@gmail.com>
2022-03-18 19:13:33 +0000

    avance 1.6

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User, Region, Rol, Habilidad_tecnica, Habilidad, Evaluacion, Profesional, Cliente
 import cloudinary.uploader
+from Flask_jwt_extended import JWTManager, get_jwtidentity, jwt_required, create_access_token
+from werkzeug.security import generate_password_hash, check_password_hash 
 
 api = Blueprint('api', __name__)
 
@@ -18,8 +20,11 @@ def registro():
         telefono = request.form['telefono'],
         direccion = request.form['direccion'],
         nombre_institucion = request.form['nombre_institucion'],
-        titulo = request.form['titulo']
+        titulo = request.form['titulo'],
+        githubuser = request.form['githubuser'],
+        empresa = request.form['empresa']
 
+        
         user = User.query.filter_by(nombre = nombre).all()
 
         if not nombre: return jsonify({ "Error": "El nombre sera requerido!"}), 400
@@ -29,7 +34,9 @@ def registro():
         if not password: return jsonify({ "Error": "La contraseña sera requerida!"}), 400
         if not telefono: return jsonify({ "Error": "El telefono sera requerido!"}), 400
         if not direccion: return jsonify({ "Error": "La direccion sera requerida!"}), 400
-
+        if not githubuser: return jsonify({ "Error": "El usuario de github sera requerido!"}), 400
+        if not empresa: return jsonify({ "msg": "El nombre de la empresa sera requerida!"}), 400
+        
         upload_avatar = cloudinary.uploader.upload(avatar,
         folder="avatar",
         public_id=nombre+"_"+avatar.filename,
@@ -44,44 +51,45 @@ def registro():
         resource_type="image"
         )
 
-        
         user = User()
         user.nombre = nombre
         user.apellido = apellido
         user.avatar = upload['secure_url']
         user.correo = correo
-        user.password = password
+        user.password = generate_password_hash(password)
         user.foto_carnet = upload['secure_url']
         user.telefono = telefono
         user.direccion = direccion
         user.nombre_institucion = nombre_institucion
         user.titulo = titulo
+        user.githubuser = githubuser
+        user.empresa = empresa
         user.save()  
+
+        profesional = Profesional()
+        profesional.githubuser = githubuser
+
+        user.profesional = profesional
+        user.save()
+
+        cliente = Cliente()
+        cliente.empresa = empresa
+
+        user.cliente = cliente
+        user.save()
+
 
 
     return jsonify(user.serialize()), 201
 
-@api.route('/Login', methods=['POST', 'GET'])
+@api.route('/Login', methods=['POST'])
 def login():
-    if request.method == 'GET':
-        response_body = {
-        "message": "W4U"
-        }
-        return jsonify(response_body), 200
 
-    if request.method == 'POST':
+    correo = request.json.get('correo'),
+    password = request.json.get('password'),
 
-        correo = request.json.get('correo'),
-        password = request.json.get('password'),
-
-        if not correo: return jsonify({ "Error": "El correo sera requerido!"}), 400
-        if not contraseña: return jsonify({ "Error": "La contraseña sera requerida!"}), 400
-
-        login = Login()
-        login.correo = correo
-        login.password = password
-
-    return jsonify(response_body), 200
+    if not correo: return jsonify({ "Error": "El correo sera requerido!"}), 400
+    if not contraseña: return jsonify({ "Error": "La contraseña sera requerida!"}), 400
 
 @api.route('/Region', methods=['POST', 'GET'])
 def region():
@@ -94,16 +102,13 @@ def region():
     if request.method == 'POST':
 
         nombre = request.json.get('nombre'),
-        regiones_id = request.json.get('regiones_id'),
 
         if not nombre: return jsonify({ "Error": "El nombre de la region sera requerida!"}), 400
-        if not regiones_id: return jsonify({ "Error": "El id de la region sera requerida!"}), 400
 
         region = Region()
         region.nombre = nombre
-        region.regiones_id = regiones_id
 
-    return jsonify(region.serialize()), 200
+    return jsonify(region.serialize()), 201
 
 @api.route('/Rol', methods=['GET', 'POST'])
 def rol():
@@ -116,16 +121,13 @@ def rol():
     if request.method == 'POST':
        
         nombre_rol = request.json.get('nombre_rol')
-        roles_id = request.json.get('roles_id')
 
         if not nombre_rol: return jsonify({ "Error": "El nombre del rol sera requerido!"}), 400
-        if not roles_id: return jsonify({ "Error": "El id del rol sera requerido!"}), 400
 
         rol = Rol()
         rol.nombre_rol = nombre_rol
-        rol.roles_id = roles_id
         
-    return jsonify(rol.serialize()), 200
+    return jsonify(rol.serialize()), 201
 
 @api.route('/Habilidad_tecnica', methods=['GET', 'POST'])
 def habilidad_tecnica():
@@ -163,16 +165,13 @@ def habilidad():
     if request.method == 'POST':
        
         nombre = request.json.get('nombre')
-#        habilidades_tecnica = request.json.get('habilidades_tecnica')
 
         if not nombre: return jsonify({ "Error": "El nombre sera requerido!"}), 400
-#        if not habilidades_id: return jsonify({ "Error": "La habilidad tecnica sera requerida!"}), 400
 
         habilidad = Habilidad()
         habilidad.nombre = nombre
-#        habilidad.habilidad_tecnica = habilidad_tecnica
       
-    return jsonify(habilidad.serialize()), 200
+    return jsonify(habilidad.serialize()), 201
 
 @api.route('/Evaluacion', methods=['GET', 'POST'])
 def evaluacion():
@@ -200,7 +199,7 @@ def evaluacion():
         evaluacion.clientes_id = clientes_id
         evaluacion.profesionales_id = profesionales_id
       
-    return jsonify(evaluacion.serialize()), 200
+    return jsonify(evaluacion.serialize()), 201
 
 @api.route('/Profesional', methods=['GET', 'POST'])
 def profesional():
@@ -214,18 +213,15 @@ def profesional():
 
         githubuser = request.json.get('githubuser')
         user_id = request.json.get('user_id')
-        profesionales_id = request.json.get('profesionales_id')
 
         if not githubuser: return jsonify({ "Error": "El usuario de github sera requerido!"}), 400
         if not user_id: return jsonify({ "Error": "El id del usuario sera requerido!"}), 400
-        if not profesionales_id: return jsonify({ "Error": "El id del profesional sera requerido!"}), 400
 
         profesional = Profesional()
         profesional.githubuser = githubuser
         profesional.user_id = user_id
-        profesional.profesionales_id = profesionales_id
 
-    return jsonify(profesional.serialize()), 200 
+    return jsonify(profesional.serialize()), 201 
 
 @api.route('/Cliente', methods=['GET', 'POST'])
 def cliente():
@@ -239,18 +235,15 @@ def cliente():
 
         empresa = request.json.get('empresa')
         user_id = request.json.get('user_id')
-        clientes_id = request.json.get('clientes_id')
 
         if not empresa: return jsonify({ "msg": "El nombre de la empresa sera requerida!"}), 400
         if not user_id: return jsonify({ "msg": "El id del usuario sera requerido!"}), 400
-        if not clientes_id: return jsonify({ "msg": "El id del cliente sera requerido!"}), 400
 
         cliente = Cliente()
         cliente.empresa = empresa
         cliente.user_id = user_id
-        cliente.clientes_id = clientes_id
 
-    return jsonify(cliente.serialize()), 200 
+    return jsonify(cliente.serialize()), 201 
 
 
 
